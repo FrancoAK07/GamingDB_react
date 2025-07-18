df42892cdb99426bb9b435c13152ac6ff3adaa28
Franco <131843647+FrancoAK07@users.noreply.github.com>
2025-07-18 14:40:50 -0400

    changed db.config.js

--- Server/config/db.config.js
+++ Server/config/db.config.js
@@ -14,13 +14,13 @@ const sequelizeConfig = {
 		acquire: 80000,
 		idle: 10000,
 	},
-	dialectOptions: {
-		ssl: {
-			ca: process.env.DB_CA,
-			rejectUnauthorized: false,
-		},
-		connectTimeout: 60000,
-	},
+	// dialectOptions: {
+	// 	ssl: {
+	// 		ca: process.env.DB_CA,
+	// 		rejectUnauthorized: false,
+	// 	},
+	// 	connectTimeout: 60000,
+	// },
 };
 
 export default sequelizeConfig;
